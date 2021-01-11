60c29f40021e1c4e71450f1cf0f34d60d2e83297
Benoit LE BOURDONNEC <benoitlebourdonnec@MacBook-Pro-2.local>
2021-01-11 20:23:31 +0100

    front jwt template

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from "axios";
 
 const Login = () => {
-  // Your code here
-  return <form />;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (email && password) {
+      Axios.post(`${process.env.REACT_APP_SERVER_ADDRESS}/login`, {
+        email,
+        password,
+      })
+        .then((res) => res.data)
+        .then((data) => {
+          localStorage.setItem("TOKEN", data.token);
+          alert("Logged Successfully");
+        })
+        .catch((err) => console.log(err.response.data.errormessage));
+    } else {
+      alert("Please specify both email and password");
+    }
+  };
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="votreemail@email.com"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </label>
+        Password:
+        <label htmlFor="password">
+          <input
+            type="text"
+            name="password"
+            id="password"
+            placeholder="*******"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </label>
+        <label htmlFor="submit">
+          <input type="submit" name="Se connecter" onClick={handleSubmit} />
+        </label>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,11 +1,30 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
 const Users = () => {
   // eslint-disable-next-line no-unused-vars
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    // Your code here
+    Axios.get(`${process.env.REACT_APP_SERVER_ADDRESS}/users`, {
+      headers: {
+        Authorization: `bearer ${localStorage.getItem("TOKEN")}`,
+      },
+    })
+      .then((res) => res.data)
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((err) => {
+        let message;
+        if (err.response.status === 401) {
+          message = "You're not authorized to access these datas";
+        } else {
+          message = err.response.data.errorMessage;
+        }
+        alert(message);
+        console.error(err);
+      });
   }, []);
 
   return (
